461d5fa1324348e9424b8786d20ce35d88949c26
kazimusabber <kazimusabber@gmail.com>
2021-08-28 22:55:00 +0600

    after update redis to cache

--- database/migrations/2021_08_27_065326_create_plugindownloads_table.php
+++ database/migrations/2021_08_27_065326_create_plugindownloads_table.php
@@ -15,14 +15,12 @@ class CreatePlugindownloadsTable extends Migration
     {
         Schema::create('plugindownloads', function (Blueprint $table) {
             $table->increments("id");
-            $table->integer("_pluginname_id")->unsigned();
+            $table->integer("_pluginname_id");
             $table->date("_download_date");
             $table->integer("_totaldownload");
             $table->timestamps();
         });
-        Schema::table('plugindownloads', function (Blueprint $table) {
-            $table->foreign("_pluginname_id")->references("id")->on("pluginnames")->nullable();
-        });
+        
     }
 
     /**
